af55a064af1f3b37685c1f0557accb5b62cb8eda
Julien Masson <julienx.masson@intel.com>
2014-08-28 16:31:18 +0200

    add dependency of magit add shortcuts for magit

--- emacs/github.el
+++ emacs/github.el
@@ -1,3 +1,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;               GITHUB CONFIG               ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; magit
+(require 'magit)
+
+;; shortcuts for magit
+(global-set-key (kbd "C-c C-g") 'magit-status)
