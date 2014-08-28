364f6f5feb24d1cbdd2818b635e6d2da5c8c269a
Julien Masson <julienx.masson@intel.com>
2014-08-28 14:54:36 +0200

    auto-connect to channel #pupdr of intel irc with the shortcut Ctrl-c e f.

--- emacs/irc.el
+++ emacs/irc.el
@@ -1,3 +1,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;                IRC CONFIG                 ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; erc
+(require 'erc)
+
+;; shortcut to connect to intel irc
+(global-set-key "\C-cef" (lambda () (interactive)
+			   (erc-tls :server "otcirc.jf.intel.com" :port "994"
+				:nick "jmasson")))
+
+;; join the #pupdr whenever connecting to intel irc.
+(setq erc-autojoin-channels-alist '(("otcirc.jf.intel.com" "#pupdr")))
