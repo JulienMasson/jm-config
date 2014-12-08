0a28e379f3c0b7f2bed7894f36e407628e3faf4a
Julien Masson <julienx.masson@intel.com>
2014-12-08 14:30:02 +0100

    Add script to setup emacs config

---
+++ others/setup_emacs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# move emacs folder
+cd ~/config/emacs/
+
+# pull all git submodules
+git submodule update --init ac-math
+git submodule update --init ample-zen
+git submodule update --init auto-complete
+git submodule update --init auto-complete-auctex
+git submodule update --init c-eldoc
+git submodule update --init dtrt-indent
+git submodule update --init emacs-ac-etags
+git submodule update --init emacs-bash-completion
+git submodule update --init erc-nick-notify
+git submodule update --init git-modes
+git submodule update --init magit
+git submodule update --init magit-filenotify
+git submodule update --init popup-el
+git submodule update --init predictive
+git submodule update --init purple
+git submodule update --init virtual-desktops.el
+git submodule update --init wanderlust
+git submodule update --init w3m
+git submodule update --init xcscope.el
+
