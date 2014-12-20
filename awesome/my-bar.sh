e2c452f0e00c2e667425fddcb8d193a5ad57a3f3
Julien Masson <massonju.eseo@gmail.com>
2014-12-20 21:31:54 +0100

    Set conky bar, the default awesome bar

--- awesome/my-bar.sh
+++ awesome/my-bar.sh
@@ -5,16 +5,31 @@ span_lo='<span color="#aaaaaa">'     #colour for normal battery
 span_sep='<span color="#ff8700">'  #colour for normal battery
 span_warn='<span color="#ff0000">'
 
-## Set battery message (works on Thinkpad Edge 11 with tp_smapi driver)
-
-# batt=`cat /sys/devices/platform/smapi/BAT0/remaining_percent`
-# if (($batt <= 30));
-# then battstate=$span_warn$batt'%% </span>'$span_lo`cat /sys/devices/platform/smapi/BAT0/state`'</span>'
-# else battstate=$span_hi$batt'%% </span>'$span_lo`cat /sys/devices/platform/smapi/BAT0/state`'</span>'
-# fi
-batt="40%"
-battstate="idle"
+# Conky
+read music mails uploadspeed downloadspeed free ip ram cpu_perc cpu_degree batt volume others <<<$(conky -c ~/jm-config/others/.conkyrc-status | tr -d ' ')
 
 ## Create the output
-
-echo ' '$battstate$span_sep' | </span>'$span_lo`cat /proc/cpuinfo | grep "cpu MHz" | awk '{sum+=$4} END { print sum/NR,"MHz"}'`'</span>'$span_sep' | </span>'$span_lo`date "+%a, %d.%m.%Y."`' - </span>'$span_hi`date +"%R"`' </span>'
+echo ' ' \
+' Music   '\
+$span_sep$music'</span>' \
+'  |  Mail   '\
+$span_sep$mails'</span>' \
+'  |  ▲   '\
+$span_sep$downloadspeed'</span>' \
+'  |  ▼   '\
+$span_sep$downloadspeed'</span>' \
+'  |  Home   '\
+$span_sep$free'</span>' \
+'  |  IP   '\
+$span_sep$ip'</span>' \
+'  |  Ram   '\
+$span_sep$ram'</span>' \
+'  |  Cpu   '\
+$span_sep$cpu_perc'%  -  '$cpu_degree'</span>' \
+'  |  Battery   '\
+$span_sep$batt'%''</span>' \
+'  |  Volume   '\
+$span_sep$volume'%''</span>' \
+'  |  ' \
+$span_lo`date "+%a, %d.%m.%Y."`'  -  </span>' \
+$span_hi`date +"%R"`' </span> '
