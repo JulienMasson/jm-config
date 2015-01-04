86697c62c73bac2767120b67dca5c1f76cffb6db
Julien Masson <massonju.eseo@gmail.com>
2015-01-04 22:43:07 +0100

    Clean my-irc.el

--- emacs/my-irc.el
+++ emacs/my-irc.el
@@ -6,27 +6,26 @@
 (require 'erc)
 
 ;; erc notification
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/elpa/erc-nick-notify-0.2.1"))
-(require 'erc-nick-notify)
+(add-to-list 'erc-modules 'notifications)
 
-;; shortcut to connect to intel irc
-(global-set-key (kbd "C-c i i") (lambda () (interactive)
-			   (erc-tls :server "otcirc.jf.intel.com" :port "994"
-				:nick "jmasson")))
-
-;; shortcut to connect to RoseTeam irc
-(global-set-key (kbd "C-c i r") (lambda () (interactive)
-			   (erc :server "irc.freenode.net" :port "6667"
-				:nick "jmasson")))
-
-;; join the #pupdr whenever connecting to intel irc.
+;; autojoin
 (require 'erc-join)
 (erc-autojoin-enable)
 (setq erc-autojoin-channels-alist
       '(("otcirc.jf.intel.com" "#pupdr")
 	("irc.freenode.net" "#RoseTeam")))
 
-;; shortcuts for magit
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;        KEYS SHORTCUTS        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (global-set-key (kbd "C-c i n") 'erc-channel-names)
+(global-set-key (kbd "C-c i i") (lambda () (interactive)
+			   (erc-tls :server "otcirc.jf.intel.com" :port "994"
+				:nick "jmasson")))
+(global-set-key (kbd "C-c i r") (lambda () (interactive)
+			   (erc :server "irc.freenode.net" :port "6667"
+				:nick "jmasson")))
 
 (provide 'my-irc)
