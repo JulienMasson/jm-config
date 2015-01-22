b4fdd97b975fd72b7903bb0ac2ad366d95aab8e5
Julien Masson <massonju.eseo@gmail.com>
2015-01-22 23:46:44 +0100

    Implement org-wl module

--- emacs/my-org.el
+++ emacs/my-org.el
@@ -5,25 +5,30 @@
 (require 'org)
 (global-set-key (kbd "C-c o l") 'org-store-link)
 (global-set-key (kbd "C-c o a") 'org-agenda)
+(global-set-key (kbd "C-c o c") 'org-capture)
 (setq org-log-done t)
 
 ;; add org agenda
-(setq org-agenda-files (list "~/org/intel.org"
-			     "~/org/notes.org"
+(setq org-agenda-files (list "~/org/notes.org"
 			     "~/org/journal.org"
-			     "~/org/todo.org"
-			     "~/org/home.org"))
+			     "~/org/todo.org"))
+
+;; org capture
+(require 'org-wl)
 
 ;; org capture for wanderlust
-(setq org-capture-templates '(("t" "Todo" entry
+(setq org-capture-templates '(("n" "Notes" entry
+			       (file+headline "~/org/notes.org" "Notes")
+			       " TODO %^{Brief Description} %^g\n%?\nAdded: %U" :prepend t)
+			      ("t" "Todo" entry
 			       (file+headline "~/org/todo.org" "Tasks")
 			       " TODO %^{Brief Description} %^g\n%?\nAdded: %U" :prepend t)
 			      ("j" "Journal" entry
 			       (file+headline "~/org/journal.org" "")
 			       "\n %^{topic} %T \n%i%?\n" :prepend t)
 			      ("e" "Email Todo" entry
-			       (file+headline "~/org/todo.org" "Tasks")
-			       "* TODO %^{Brief Description}\n%a\n%?Added: %U\n" :prepend t)
+			       (file+headline "~/org/todo.org" "Mails")
+			       "* TODO %^{Brief Description}\nEmail: %a\nFrom: %:from \nTo: %:to \n%?Added: %U\n" :prepend t)
 			      ))
 
 (provide 'my-org)
