5b203d0c241ed184c8af09c3af055c4f50c47190
Julien Masson <massonju.eseo@gmail.com>
2015-02-04 22:35:45 +0100

    Change org config
    Conflicts:
    	emacs/modules/offlineimap
    	emacs/modules/virtual-desktops
    	emacs/modules/xcscope
    	emacs/my-org.el

--- emacs/my-org.el
+++ emacs/my-org.el
@@ -15,17 +15,14 @@
 
 ;; org capture for wanderlust
 (setq org-capture-templates '(("n" "Notes" entry
-			       (file+headline "~/org/notes.org" "Notes")
-			       " TODO %^{Brief Description} %^g\n%?\nAdded: %U" :prepend t)
-			      ("t" "Todo" entry
-			       (file+headline "~/org/todo.org" "Tasks")
+			       (file+headline "~/org/notes.org" "Extra")
 			       " TODO %^{Brief Description} %^g\n%?\nAdded: %U" :prepend t)
 			      ("l" "Link" plain
 			       (file "~/org/notes.org" "Links")
 			       "- %?\n %x\n")
 			      ("t" "Todo" entry
 			       (file+headline "~/org/todo.org" "Tasks")
-			       "* TODO %?\n %i\n")
+			       " TODO %^{Brief Description} %^g\n%?\nAdded: %U" :prepend t)
 			      ("j" "Journal" entry
 			       (file+datetree "~/org/journal.org")
 			       "* %?\nEntered on %U\n  %i\n  %a")
