0173ccd2f60a02866ea6a45a78a5daa262bcc0ab
Julien Masson <massonju.eseo@gmail.com>
2015-02-15 19:23:40 +0100

    Add others modules for wanderlust

--- emacs/my-wl.el
+++ emacs/my-wl.el
@@ -20,6 +20,7 @@
      ))
 
 ;; wanderlust
+(require 'wl)
 (autoload 'wl "wl" "Wanderlust" t)
 (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
 (autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
