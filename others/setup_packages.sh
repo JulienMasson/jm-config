80dfeee33808d5afa25f7578fe3598acfb992a4d
JulienMasson <massonju.eseo@gmail.com>
2015-02-22 19:24:15 +0100

    Update setup management and delete i3 config

--- others/setup_packages.sh
+++ others/setup_packages.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# emacs
-sudo apt-get install emacs -y
+# packages for compiling emacs and modules
+sudo apt-get install build-essential autogen autoconf automake libtool texinfo libc6-dev libncurses5-dev libpng-dev xaw3dg-dev zlib1g-dev libice-dev libsm-dev libx11-dev libxext-dev libxi-dev libxmu-dev libxmuu-dev libxpm-dev libxrandr-dev libxt-dev libxtst-dev libxv-dev libgif-dev libtiff5-dev libgtk-3-dev libncurses5-dev libgtk2.0-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libtiff4-dev libxml2-dev librsvg2-dev libotf-dev libm17n-dev libgpm-dev libgnutls-dev libgconf2-dev libdbus-1-dev libgmime-2.6-dev libxapian-dev -y
 
-# wl
-sudo apt-get install wl -y
-
-# cscope ctags
-sudo apt-get install cscope exuberant-ctags -y
-
-# zsh
-sudo apt-get install zsh -y
-
-# conky
-sudo apt-get install conky -y
+# cscope
+sudo apt-get install cscope -y
 
 # dmenu
 sudo apt-get install dmenu -y
@@ -35,6 +26,9 @@ sudo apt-get install offlineimap -y
 
 # lxappearance
 sudo apt-get install lxappearance -y
+
+# xscreensaver
+sudo apt-get install xscreensaver -y
 
 # oocairo
 cd ~/jm-config/awesome/modules/
