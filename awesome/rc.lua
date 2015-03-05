9262c133d482c0675f195d504a567d8a0d5cb6f9
Julien Masson <massonju.eseo@gmail.com>
2015-03-05 21:34:00 +0100

    Add labtop awesome config

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,3 +1,4 @@
 local home_dir   = os.getenv("HOME")
 dofile(home_dir .. "/jm-config/awesome/rc-home-pc.lua")
+-- dofile(home_dir .. "/jm-config/awesome/rc-home-labtop.lua")
 -- dofile(home_dir .. "/jm-config/awesome/rc-work.lua")
