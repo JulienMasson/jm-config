8cea640847469bb1e44e38ac609b238a96dcf39f
Julien Masson <massonju.eseo@gmail.com>
2015-03-10 23:32:24 +0100

    Add home gnus config

---
+++ emacs/home/home-gnus.el
@@ -0,0 +1,65 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;               GNUS CONFIG                 ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; set maildir
+(setq gnus-select-method
+      '(nnmaildir "OpenWide"
+                  (directory "~/Maildir/OpenWide")
+                  (directory-files nnheader-directory-files-safe)
+                  (get-new-mail nil)))
+
+;; (setq gnus-select-method
+;;       '(nnmaildir "Eseo"
+;;                   (directory "~/Maildir/Eseo")
+;;                   (directory-files nnheader-directory-files-safe)
+;;                   (get-new-mail nil)))
+
+;; (setq gnus-select-method
+;;       '(nnmaildir "Gmail"
+;;                   (directory "~/Maildir/Gmail")
+;;                   (directory-files nnheader-directory-files-safe)
+;;                   (get-new-mail nil)))
+
+
+;; eye candy
+(copy-face 'font-lock-variable-name-face 'gnus-face-6)
+(setq gnus-face-6 'gnus-face-6)
+(copy-face 'font-lock-constant-face 'gnus-face-7)
+(setq gnus-face-7 'gnus-face-7)
+(copy-face 'gnus-face-7 'gnus-summary-normal-unread)
+(copy-face 'font-lock-constant-face 'gnus-face-8)
+(set-face-foreground 'gnus-face-8 "gray50")
+(setq gnus-face-8 'gnus-face-8)
+(copy-face 'font-lock-constant-face 'gnus-face-9)
+(set-face-foreground 'gnus-face-9 "gray70")
+(setq gnus-face-9 'gnus-face-9)
+(setq gnus-summary-make-false-root 'dummy)
+(setq gnus-summary-make-false-root-always nil)
+(defun oxy-unicode-threads ()
+  (interactive)
+  (setq gnus-summary-dummy-line-format "    %8{│%}   %(%8{│%}                       %7{│%}%) %6{□%}  %S\n"
+	gnus-summary-line-format "%8{%4k│%}%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{│%} %6{%B%} %s\n"
+	gnus-sum-thread-tree-indent " "
+	gnus-sum-thread-tree-root "■ "
+	gnus-sum-thread-tree-false-root "□ "
+	gnus-sum-thread-tree-single-indent "▣ "
+	gnus-sum-thread-tree-leaf-with-other "├─▶ "
+	gnus-sum-thread-tree-vertical "│"
+	gnus-sum-thread-tree-single-leaf "└─▶ "))
+
+(defun oxy-unicode-threads-heavy ()
+  (interactive)
+  (setq gnus-summary-line-format "%8{%4k│%}%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{║%} %6{%B%} %s\n"
+	gnus-summary-dummy-line-format "    %8{│%}   %(%8{│%}                       %7{║%}%) %6{┏○%}  %S\n"
+	gnus-sum-thread-tree-indent " "
+	gnus-sum-thread-tree-root "┏● "
+	gnus-sum-thread-tree-false-root " ○ "
+	gnus-sum-thread-tree-single-indent " ● "
+	gnus-sum-thread-tree-leaf-with-other "┣━━❯ "
+	gnus-sum-thread-tree-vertical "┃"
+	gnus-sum-thread-tree-single-leaf "┗━━❯ "))
+
+(oxy-unicode-threads-heavy)
+
+(provide 'home-gnus)
