a31481f99bd8224bd68d24197464416b231d1af8
Julien Masson <massonju.eseo@gmail.com>
2015-08-21 11:58:10 +0200

    Change grep command and add ggtags support

--- emacs/my-programming.el
+++ emacs/my-programming.el
@@ -103,7 +103,7 @@
 	    (dtrt-indent-mode t)))
 
 ;; change default grep
-(setq grep-command "grep -nrH -e ")
+(setq grep-command "grep --color -nrH -e ")
 
 ;; auto complete config
 (require 'auto-complete-config)
@@ -111,9 +111,20 @@
 (ac-config-default)
 
 ;; auto complete based on ggtags
-(require 'setup-ggtags)
+(require 'ggtags)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+              (ggtags-mode 1))))
 (set-face-foreground 'ac-gtags-candidate-face "coral3")
 (set-face-background 'ac-gtags-selection-face nil)
+(define-key ggtags-mode-map (kbd "C-c t s") 'ggtags-find-other-symbol)
+(define-key ggtags-mode-map (kbd "C-c t h") 'ggtags-view-tag-history)
+(define-key ggtags-mode-map (kbd "C-c t r") 'ggtags-find-reference)
+(define-key ggtags-mode-map (kbd "C-c t f") 'ggtags-find-file)
+(define-key ggtags-mode-map (kbd "C-c t c") 'ggtags-create-tags)
+(define-key ggtags-mode-map (kbd "C-c t u") 'ggtags-update-tags)
+(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
 
 ;; locate database
 (defvar jm-cmd-locate-database
