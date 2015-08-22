4967733657e48281ccaa5ba7c58eceb3e9c6f731
Julien Masson <julien@localhost.localdomain>
2015-08-22 03:07:00 +0200

    Update github config according magit 2 !!!!

--- emacs/my-github.el
+++ emacs/my-github.el
@@ -5,6 +5,25 @@
 ;; magit
 (require 'magit)
 (require 'magit-blame)
+(setq magit-last-seen-setup-instructions "1.4.0")
+(setq magit-git-executable "~/bin/git-install/bin/git")
+(setq vc-handled-backends nil)
+
+(define-key magit-mode-map [remap magit-copy-buffer-thing-as-kill] 'kill-ring-save)
+
+(custom-set-faces
+ '(magit-section-highlight ((t)))
+ '(magit-section-heading ((t :inherit font-lock-keyword-face)))
+ '(magit-hash ((t :foreground "DarkSlateGrey")))
+ '(magit-diff-file-heading ((t)))
+ '(magit-diff-added ((t :inherit diff-added)))
+ '(magit-diff-added-highlight ((t :inherit diff-added :background "grey4")))
+ '(magit-diff-removed ((t :inherit diff-removed)))
+ '(magit-diff-removed-highlight ((t :inherit diff-removed :background "grey4")))
+ '(magit-diff-file-heading-highlight ((t :inherit (magit-diff-file-heading secondary-selection))))
+ '(magit-diff-hunk-heading-highlight ((t :inherit diff-hunk-header)))
+ '(magit-diff-context-highlight ((t :inherit diff-hunk-header)))
+ '(magit-log-date ((t :foreground "DarkSlateGrey"))))
 
 ;; shortcut to know if branch is up to date
 (setq path-to-check-git-update "~/jm-config/others/check_git_update.sh")
--- emacs/my-keybindings.el
+++ emacs/my-keybindings.el
@@ -66,12 +66,12 @@
 
 ;; magit
 (global-set-key (kbd "C-c g s") 'magit-status)
-(global-set-key (kbd "C-c g l") 'magit-log)
-(global-set-key (kbd "C-c g f") 'magit-file-log)
+(global-set-key (kbd "C-c g l") 'magit-log-current)
+(global-set-key (kbd "C-c g f") 'magit-log-buffer-file)
 (global-set-key (kbd "C-c g p") 'magit-pull)
-(global-set-key (kbd "C-c g r") 'magit-reset-head-hard)
+(global-set-key (kbd "C-c g r") 'magit-reset-hard)
 (global-set-key (kbd "C-c g a") 'magit-commit-amend)
-(global-set-key (kbd "C-c g b") 'magit-blame-mode)
+(global-set-key (kbd "C-c g b") 'magit-blame)
 (global-set-key (kbd "C-c g c") 'magit-checkout)
 (global-set-key (kbd "C-c g n") 'magit-create-branch)
 (global-set-key (kbd "C-c g d") 'magit-delete-branch)
