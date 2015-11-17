61949b853eebde513c3c12ad42e9b60e4fb78872
Julien Masson <massonju.eseo@gmail.com>
2015-11-17 18:02:13 +0100

    remap magit-copy-buffer-revision to kill-ring-save

--- emacs/my-github.el
+++ emacs/my-github.el
@@ -12,6 +12,7 @@
 (add-hook 'git-commit-setup-hook 'git-commit-turn-on-flyspell)
 
 (define-key magit-mode-map [remap magit-copy-buffer-thing-as-kill] 'kill-ring-save)
+(define-key magit-mode-map [remap magit-copy-buffer-revision] 'kill-ring-save)
 
 ;; shortcut to know if branch is up to date
 (setq path-to-check-git-update "~/jm-config/others/check_git_update.sh")
