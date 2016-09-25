389f160eacbe215d0146679dc44b36ed501f4ead
Julien Masson <massonju.eseo@gmail.com>
2016-09-25 22:34:31 +0200

    Add lua mode
    Signed-off-by: Julien Masson <massonju.eseo@gmail.com>

--- emacs/my-programming.el
+++ emacs/my-programming.el
@@ -29,6 +29,12 @@
 			     uncrustify-files))
     (message (propertize "No files selected" 'face 'error))))
 
+;; lua mode
+(require 'lua-mode)
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
 ;; cscope
 (require 'xcscope)
 (cscope-setup)
