446f49db2861d72752bf82407e70c82e8ce9259f
Julien Masson <massonju.eseo@gmail.com>
2016-09-27 22:56:39 +0200

    Add arandr and pulseaudio in basic packages
    Signed-off-by: Julien Masson <massonju.eseo@gmail.com>

--- tools/setup.sh
+++ tools/setup.sh
@@ -22,11 +22,11 @@
 cd ~
 mkdir bin Documents Downloads Pictures Music Desktop .config
 
-# basic package
+# basic packages
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-sudo apt-get install xinit cscope slock numlockx offlineimap alsa-utils dmenu aptitude pcmanfm texlive iceweasel flashplugin-nonfree evince mairix eog install-info -y
+sudo apt-get install xinit arandr cscope slock numlockx offlineimap alsa-utils pulseaudio dmenu aptitude pcmanfm texlive iceweasel flashplugin-nonfree evince mairix eog install-info -y
 
 # packages for emacs
 sudo apt-get install build-essential autogen autoconf automake libtool texinfo libc6-dev libncurses5-dev libpng-dev xaw3dg-dev zlib1g-dev libice-dev libsm-dev libx11-dev libxext-dev libxi-dev libxmu-dev libxmuu-dev libxpm-dev libxrandr-dev libxt-dev libxtst-dev libxv-dev libgif-dev libtiff5-dev libgtk-3-dev libncurses5-dev libgtk2.0-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libtiff5-dev libxml2-dev librsvg2-dev libotf-dev libm17n-dev libgpm-dev libgconf2-dev libdbus-1-dev libgmime-2.6-dev libxapian-dev gnutls-dev -y
