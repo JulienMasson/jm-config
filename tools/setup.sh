99fa11ca85d3d864423228c8ad7f862baebc62e9
Julien Masson <julienx.masson@intel.com>
2017-12-06 17:18:16 -0800

    Update setup, remove useless files and autodetect dual-screen
    Signed-off-by: Julien Masson <julienx.masson@intel.com>

--- tools/setup.sh
+++ tools/setup.sh
@@ -34,9 +34,13 @@
 # $ iwconfig && ifconfig
 # $ sudo dhclient wlan0
 #
-# - clean recompile emacs
-# $ make distclean
-# $ make all
+# - run sudo command with no password:
+# $ sudo visudo
+# %sudo   ALL=(ALL:ALL) NOPASSWD: ALL
+#
+# - Reduce timeout of grub: /etc/default/grub
+# GRUB_TIMEOUT=0
+# $ sudo update-grub
 #
 # - update all git submodules on master
 # $ git submodule foreach git checkout master
@@ -50,16 +54,16 @@ mkdir bin Documents Downloads Pictures Music Desktop .config
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-sudo apt-get install xinit rxvt-unicode arandr cscope slock numlockx offlineimap alsa-utils pulseaudio dmenu aptitude thunar texlive flashplugin-nonfree evince mairix eog install-info -y
+sudo apt-get install xinit rxvt-unicode arandr cscope offlineimap alsa-utils pulseaudio rofi i3lock-fancy dunst tint2 thunar evince mairix eog install-info -y
 
 # OPTIONAL packages
-sudo apt-get install pv lxappearance network-manager-gnome numix-gtk-theme numix-icon-theme gnome-system-monitor -y
+sudo apt-get install aptitude pv lxappearance network-manager-gnome numix-gtk-theme numix-icon-theme gnome-system-monitor -y
 
 # packages for emacs
-sudo apt-get install build-essential autogen autoconf automake libtool texinfo libc6-dev libncurses5-dev libpng-dev xaw3dg-dev zlib1g-dev libice-dev libsm-dev libx11-dev libxext-dev libxi-dev libxmu-dev libxmuu-dev libxpm-dev libxrandr-dev libxt-dev libxtst-dev libxv-dev libgif-dev libtiff5-dev libgtk-3-dev libncurses5-dev libgtk2.0-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libtiff5-dev libxml2-dev librsvg2-dev libotf-dev libm17n-dev libgpm-dev libgconf2-dev libdbus-1-dev libgmime-2.6-dev libxapian-dev gnutls-dev -y
+sudo apt-get install build-essential autogen autoconf automake libtool texinfo libc6-dev libncurses5-dev libpng-dev xaw3dg-dev zlib1g-dev libice-dev libsm-dev libx11-dev libxext-dev libxi-dev libxmu-dev libxmuu-dev libxpm-dev libxrandr-dev libxt-dev libxtst-dev libxv-dev libgif-dev libtiff5-dev libgtk-3-dev libncurses5-dev libgtk2.0-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libtiff5-dev libxml2-dev librsvg2-dev libotf-dev libm17n-dev libgpm-dev libgconf2-dev libdbus-1-dev libgmime-2.6-dev libxapian-dev gnutls-dev libmagickcore-dev libmagick++-dev -y
 
 # packages for jwm
-sudo apt-get install libx11-xcb-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-randr0-dev
+sudo apt-get install libx11-xcb-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-keysyms1-dev
 
 # install firefox
 pushd Downloads
