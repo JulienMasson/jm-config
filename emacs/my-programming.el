a2a588b07bc30a6fee98638a90adcb26e0e38d74
Julien Masson <julienx.masson@intel.com>
2017-12-06 17:24:50 -0800

    Remove markdown-mode
    Signed-off-by: Julien Masson <julienx.masson@intel.com>

--- emacs/my-programming.el
+++ emacs/my-programming.el
@@ -10,8 +10,6 @@
 
 ;; markdown mode
 (require 'markdown-mode)
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
