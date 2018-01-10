2c47edeeb2af23cde88f303def59c274daacc7e1
Julien Masson <julienx.masson@intel.com>
2018-01-10 12:27:51 -0800

    Cleanup programming and bindings
    Signed-off-by: Julien Masson <julienx.masson@intel.com>

--- emacs/my-keybindings.el
+++ emacs/my-keybindings.el
@@ -25,52 +25,42 @@
 (global-set-key (kbd "M-2") (lambda () (interactive) (virtual-desktops-goto 2)))
 (global-set-key (kbd "M-3") (lambda () (interactive) (virtual-desktops-goto 3)))
 (global-set-key (kbd "M-4") (lambda () (interactive) (virtual-desktops-goto 4)))
-(global-set-key (kbd "M-5") (lambda () (interactive) (virtual-desktops-goto 5)))
-(global-set-key (kbd "M-6") (lambda () (interactive) (virtual-desktops-goto 6)))
-(global-set-key (kbd "M-7") (lambda () (interactive) (virtual-desktops-goto 7)))
-(global-set-key (kbd "M-8") (lambda () (interactive) (virtual-desktops-goto 8)))
-(global-set-key (kbd "M-9") (lambda () (interactive) (virtual-desktops-goto 9)))
 
 ;; org timer shortcuts
-(global-set-key (kbd "C-c t s") 'org-timer-start)
-(global-set-key (kbd "C-c t p") 'org-timer-pause-or-continue)
-(global-set-key (kbd "C-c t e") 'org-timer-stop)
-(global-set-key (kbd "C-c o l") 'org-store-link)
 (global-set-key (kbd "C-c o a") 'org-agenda-list)
 (global-set-key (kbd "C-c o t") 'org-todo-list)
 (global-set-key (kbd "C-c o c") 'org-capture)
-
-;; shortcuts for pidgin using purple
-(global-set-key (kbd "C-c p i") 'purple-init)
-(global-set-key (kbd "C-c p b") 'purple-buddies-list)
-(global-set-key (kbd "C-c p s") 'purple-status-set)
-(global-set-key (kbd "C-c p m") 'purple-mail-to)
-(global-set-key (kbd "C-c p a") 'purple-buddy-add)
-(global-set-key (kbd "C-c p j") 'purple-chat-jump)
-
-;; perl
-(global-set-key (kbd "C-c p d") 'perldoc)
 
 ;; magit
 (global-set-key (kbd "C-c g s") 'magit-status)
 (global-set-key (kbd "C-c g l") 'magit-log-head)
 (global-set-key (kbd "C-c g f") 'magit-log-buffer-file)
-(global-set-key (kbd "C-c g p") 'magit-pull)
 (global-set-key (kbd "C-c g r") 'magit-reset-hard)
-(global-set-key (kbd "C-c g a") 'magit-commit-amend)
 (global-set-key (kbd "C-c g b") 'magit-blame)
-(global-set-key (kbd "C-c g c") 'magit-checkout)
-(global-set-key (kbd "C-c g n") 'magit-create-branch)
-(global-set-key (kbd "C-c g d") 'magit-delete-branch)
-(global-set-key (kbd "C-c g m") 'magit-branch-manager)
-(global-set-key (kbd "C-c g u") 'check-git-branch-update)
 
-;; xcsope
-(global-set-key (kbd "C-c c d") 'jm-cscope-database)
-(global-set-key (kbd "C-c c s") 'jm-switch-cscope-database)
+;; compilation
+(global-set-key (kbd "C-c SPC") (lambda () (interactive) (with-current-buffer "*compilation*" (recompile))))
 
-;; locate
-(global-set-key (kbd "C-c C-l") 'jm-search-locate-database)
+;; open browser
+(global-set-key (kbd "C-c C-o") 'browse-url)
+
+;; refresh status
+(global-set-key (kbd "C-c C-u") (lambda () (interactive) (run-at-time 1 status-refresh-timer-delay 'status-update)))
+
+;; bitlbee
+(global-set-key (kbd "C-c e b") 'bitlbee-update-list)
+(global-set-key (kbd "C-c e c") 'bitlbee-chat)
+(global-set-key (kbd "C-c e j") 'bitlbee-jump)
+
+;; cscope
+(define-key cscope-minor-mode-keymap  "\C-csa" 'cscope-add-cscope-search-list)
+(define-key cscope-minor-mode-keymap  "\C-csr" 'cscope-reset-cscope-search-list)
+
+;; map TAB to company completion in shell mode
+(define-key shell-mode-map (kbd "TAB") #'company-manual-begin)
+
+;; pycscope
+(define-key cscope-minor-mode-keymap (kbd "C-c s p") 'cscope-pycscope)
 
 
 
--- emacs/my-programming.el
+++ emacs/my-programming.el
@@ -23,40 +23,15 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 ;; default indentation
-(setq c-default-style (quote ((awk-mode . "awk") (c-mode . "linux") (other . "gnu"))))
+(setq c-default-style '((c-mode . "linux") (other . "gnu")))
 
 ;; save backup files
 (setq backup-directory-alist `(("." . "~/.saves")))
 
-;; use uncrustify with dired mode
-(setq uncrustify-config-path "~/bin/uncrustify/etc/linux.cfg")
-(setq uncrustify-bin "~/bin/uncrustify/src/uncrustify")
-(setq uncrustify-args "--no-backup")
-
-(defun uncrustify-dired ()
-  (interactive)
-  (setq uncrustify-files (mapconcat 'identity
-				    (mapcar 'untramp-path (dired-get-marked-files))
-				    " "))
-  (if (not (string= uncrustify-files ""))
-      (shell-command (format "%s -c %s %s %s"
-			     uncrustify-bin
-			     uncrustify-config-path
-			     uncrustify-args
-			     uncrustify-files))
-    (message (propertize "No files selected" 'face 'error))))
-
 ;; cscope
 (require 'xcscope)
-(setq cscope-option-do-not-update-database t)
 (setq cscope-option-use-inverted-index t)
 (cscope-setup)
-(setq path-to-cscope "/usr/bin/cscope")
-(defun create-tags-cscope (dir-name)
-  "Create tags file."
-  (interactive "DDirectory: ")
-  (shell-command
-   (format "cd %s; find . -name \"*.c\" -o -name \"*.cpp\" -o -name \"*.h\" > cscope.files; %s -q -R -b -i cscope.files" (directory-file-name dir-name) path-to-cscope)))
 
 ;; cscope search list
 (defvar jm-cscope-search-list nil)
@@ -69,10 +44,6 @@
   "Rest cscope search list"
   (interactive)
   (setq jm-cscope-search-list nil))
-
-(define-key cscope-minor-mode-keymap  "\C-csa" 'cscope-add-cscope-search-list)
-(define-key cscope-minor-mode-keymap  "\C-csr" 'cscope-reset-cscope-search-list)
-
 
 ;; auto-detection indenting
 (require 'dtrt-indent)
@@ -87,9 +58,6 @@
 ;; company mode
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
-
-;; map TAB to company completion in shell mode
-(define-key shell-mode-map (kbd "TAB") #'company-manual-begin)
 
 ;; additionnals company backends
 (require 'company-c-headers)
@@ -114,7 +82,6 @@
   (let ((default-directory dir))
     (async-shell-command "pycscope -D .")))
 
-(define-key cscope-minor-mode-keymap (kbd "C-c s p") 'cscope-pycscope)
 (add-hook 'python-mode-hook (function cscope-minor-mode))
 
 (provide 'my-programming)
