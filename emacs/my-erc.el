83ff8187654a9c02a5253949da6041dadf64814e
Julien Masson <julienm@fb.com>
2018-03-09 12:08:12 -0800

    erc: save message in ~/.erc/logs/
    Signed-off-by: Julien Masson <julienm@fb.com>

--- emacs/my-erc.el
+++ emacs/my-erc.el
@@ -27,5 +27,15 @@
 ;; exclude some types of messages
 (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"))
 
+;; logging
+(erc-log-mode)
+(setq erc-log-channels-directory "~/.erc/logs/")
+(setq erc-generate-log-file-name-function (quote erc-generate-log-file-name-with-date))
+(setq erc-save-buffer-on-part nil)
+(setq erc-save-queries-on-quit nil)
+(setq erc-log-write-after-insert t)
+(setq erc-log-write-after-send t)
+(setq erc-log-insert-log-on-open t)
+
 
 (provide 'my-erc)
