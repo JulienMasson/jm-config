f68246efc1abd8764f7d739cb2a2aad9393dfde4
Julien Masson <julienm@fb.com>
2018-04-05 15:38:21 -0700

    Remove erc-view-log and apply face on log
    Signed-off-by: Julien Masson <julienm@fb.com>

--- emacs/my-erc.el
+++ emacs/my-erc.el
@@ -30,19 +30,39 @@
 ;; logging
 (erc-log-mode)
 (setq erc-log-channels-directory "~/.erc/logs/")
-(setq erc-generate-log-file-name-function (quote erc-generate-log-file-name-with-date))
+(setq erc-generate-log-file-name-function (quote erc-generate-log-file-name-short))
 (setq erc-save-buffer-on-part nil)
 (setq erc-save-queries-on-quit nil)
 (setq erc-log-write-after-insert t)
 (setq erc-log-write-after-send t)
 (setq erc-log-insert-log-on-open t)
 
-;; erc view log
-(require 'erc-view-log)
-(add-to-list 'auto-mode-alist
-	     `(,(format "%s/.*\\.log"
-			(regexp-quote (expand-file-name erc-log-channels-directory))) . erc-view-log-mode))
-(add-hook 'erc-view-log-mode-hook 'turn-on-auto-revert-tail-mode)
+;; apply face on log
+(defun erc-log-match-face ()
+  (list
+   ;; own message line
+   `(,(format "^\\(<\\)\\(%s\\)\\(>\\)[ \n]\\(%s\\)$" "jmasson" erc-view-log-message-regexp)
+     (1 'erc-default-face)
+     (2 'erc-my-nick-face)
+     (3 'erc-default-face)
+     (4 'erc-input-face))
+   ;; standard message line
+   `(,(format "^\\(<\\)\\(%s\\)\\(>\\)[ \n]\\(%s\\)$" erc-view-log-nickname-regexp erc-view-log-message-regexp)
+     (1 'erc-default-face)
+     (2 (erc-log-nick-get-face (match-string 3)))
+     (3 'erc-default-face)
+     (4 'erc-default-face))
+   ;; *** message line
+   `("^\\(\\*\\*\\*.*\\)"
+     (1 'erc-timestamp-face))
+   ;; [date] message line
+   `("^\\(\\[.*\\]\\)"
+     (1 'erc-timestamp-face))))
+
+(defun erc-apply-face-on-log ()
+  (setq font-lock-defaults `(,(erc-log-match-face))))
+
+(advice-add 'erc-mode :before #'erc-apply-face-on-log)
 
 
 (provide 'my-erc)
