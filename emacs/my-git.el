8533ad029447f09ce2a010b7778329bfe494170b
Julien Masson <massonju.eseo@gmail.com>
2019-05-27 11:12:19 +0200

    git: remove push gerrit feature, obsolete and unused
    Signed-off-by: Julien Masson <massonju.eseo@gmail.com>

--- emacs/my-git.el
+++ emacs/my-git.el
@@ -151,29 +151,6 @@
 ;; set signoff by default
 (setq transient-values '((magit-commit "--signoff")))
 
-;; magit push gerrit
-(defun magit-git-push-gerrit (branch target args)
-  (run-hooks 'magit-credential-hook)
-  (-let [(remote . target)
-         (magit-split-branch-name target)]
-    (magit-run-git-async "push" "-v" args remote
-                         (format "%s:refs/for/%s" branch target))))
-
-(defun magit-push-gerrit (source target args)
-  "Push an arbitrary branch or commit somewhere.
-Both the source and the target are read in the minibuffer."
-  (interactive
-   (let ((source (magit-read-local-branch-or-commit "Push")))
-     (list source
-           (magit-read-remote-branch (format "Push %s to" source) nil
-                                     (magit-get-upstream-branch source)
-                                     source 'confirm)
-           (magit-push-arguments))))
-  (magit-git-push-gerrit source target args))
-
-(magit-define-popup-action 'magit-push-popup
-  ?g "Gerrit" 'magit-push-gerrit)
-
 ;; magit log from HEAD to last Tag found
 (defun magit-log-from-head-to-last-tag (&optional args files)
   "Show log from `HEAD' to last Tag found."
