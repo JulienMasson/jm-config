5147e439c5a5437b13c97f413b748f120da2c390
Julien Masson <massonju.eseo@gmail.com>
2022-04-26 18:12:43 +0200

    keybindings: add magit-toggle-buffer-lock + sort
    Signed-off-by: Julien Masson <massonju.eseo@gmail.com>

--- emacs/keybindings-extra.el
+++ emacs/keybindings-extra.el
@@ -79,14 +79,15 @@
 (global-set-key (kbd "C-c C-o") 'browse-url)
 
 ;; magit
-(global-set-key (kbd "C-c g s") 'magit-status)
-(global-set-key (kbd "C-c g l") 'magit-log-head)
-(global-set-key (kbd "C-c g f") 'magit-log-buffer-file)
-(global-set-key (kbd "C-c g d") 'magit-log-directory)
-(global-set-key (kbd "C-c g r") 'magit-reset-hard-head)
 (global-set-key (kbd "C-c g b") 'magit-blame-addition)
-(global-set-key (kbd "C-c g y") 'magit-show-refs)
 (global-set-key (kbd "C-c g c") 'my-magit-show-commit)
+(global-set-key (kbd "C-c g d") 'magit-log-directory)
+(global-set-key (kbd "C-c g f") 'magit-log-buffer-file)
+(global-set-key (kbd "C-c g l") 'magit-log-head)
+(global-set-key (kbd "C-c g r") 'magit-reset-hard-head)
+(global-set-key (kbd "C-c g s") 'magit-status)
+(global-set-key (kbd "C-c g t") 'magit-toggle-buffer-lock)
+(global-set-key (kbd "C-c g y") 'magit-show-refs)
 (define-key magit-mode-map [remap magit-copy-buffer-thing-as-kill] 'kill-ring-save)
 (define-key magit-mode-map [remap magit-copy-buffer-revision] 'kill-ring-save)
 
